c37e60598067893deb0969ed96fe0e72edbeed3c
Nikita Sychev <nikitasychev@inbox.ru>
2023-11-12 16:00:19 +0500

    Add project files.

---
+++ DataBase.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2_DB_Projects
+{
+    abstract class DataBase
+    {
+        public string connectionString;
+
+        public abstract void OpenConnection();
+        public abstract void CloseConnection();
+      
+
+    }
+}
