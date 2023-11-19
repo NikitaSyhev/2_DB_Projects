2d1bbf052328e36eefccc144d3a8af75aa418a85
Nikita Sychev <nikitasychev@inbox.ru>
2023-11-19 12:15:59 +0500

    message box

--- SQLiteDB.cs
+++ SQLiteDB.cs
@@ -11,22 +11,26 @@ namespace _2_DB_Projects
 {
     public class SQLiteDB:DataBase
     {
-        private SQLiteConnection connection = new SQLiteConnection(@"Data Source=Users.db;Version=3;");
+        private SQLiteConnection connection = new SQLiteConnection(@"Data Source=WareHouse.db;Version=3;");
         public string query = "";
         
-        public SQLiteDB(string constr) : base(constr) 
-        {
-
-
-            
-        }
+        public SQLiteDB(string constr) : base(constr) {}
 
         public override void OpenConnection() // безопасное открытие БД с проверкой состояния
         {
             if (connection.State == System.Data.ConnectionState.Closed)
             {
-                connection.Open();
-                MessageBox.Show("Connection Open");
+                try
+                {
+                    connection.Open();
+                    MessageBox.Show("Connection Open");
+                }
+                catch (Exception ex)
+                {
+
+                    MessageBox.Show($"Ошибка {ex.Message}");
+                }
+               
                 
             }
 
@@ -39,6 +43,7 @@ namespace _2_DB_Projects
             if (connection.State==ConnectionState.Open)
             {
                 connection.Close();
+                MessageBox.Show("Connection Close");
             }
         }
         public override DataTable ExecuteQuerry(string querry)
